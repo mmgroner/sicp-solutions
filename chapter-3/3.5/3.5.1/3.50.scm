11db51d2cf9100e269451c7e7ed7eb403c506d7c
MegaSpaceHamlet <mendlegr@gmail.com>
2024-04-02 23:02:56 -0400

    3.5 - Streams. 3.50

---
+++ chapter-3/3.5/3.5.1/3.50.scm
@@ -0,0 +1,13 @@
+#lang sicp
+(#%require "../streams.scm")
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+; in action
+(define integers (cons-stream 1 (stream-map + ones integers)))
---
+++ chapter-3/3.5/streams.scm
@@ -0,0 +1,53 @@
+#lang sicp
+(#%provide (all-defined))
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream low (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred s)
+  (if (stream-null? s)
+      the-empty-stream
+      (if (pred (stream-car s))
+          (cons-stream (stream-car s) (stream-filter pred (stream-cdr s)))
+          (stream-filter pred (stream-cdr s)))))
+
+
+; infinite loop alert!
+(define (display-stream s)
+  (stream-for-each display-line s))
+(define (display-line x) (newline) (display x))
+
+; show stream
+(define (ss stream)
+  (define (step s n)
+    (cond ((stream-null? s) (newline))
+          ((< n 10) (begin
+                      (display (stream-car s))
+                      (step (stream-cdr s) (+ n 1))))
+          (else '...)))
+  (step stream 0))
+
+; some common streams to play with
+(define ones (cons-stream 1 ones))
