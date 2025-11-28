005024bfe1fe88a200f9210a9002d78187ddcf3d
Mendel Groner <mendlegr@gmail.com>
2025-11-28 09:56:02 -0500

    feat(4.6): refined `let->combination` procedure
    - Correctly handle multi-line lambda bodies
    - Use `make-lambda` for enhanced abstraction and readability

--- chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/4.6.rkt
+++ chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/4.6.rkt
@@ -2,24 +2,40 @@
 (provide let->combination)
 (provide let?)
 
-(define (let-definitions-clause exp) (cadr exp))
-(define (let-body exp) (caddr exp))
-(define (let-parameters definitions-clause) (map car definitions-clause))    
-(define (let-argument-expressions definitions-clause) (map cadr definitions-clause))
+; get the definitions part of the let construct
+(define (let-definitions-clause exp)
+  (cadr exp))
 
+; get the lambda body part of the let constrcut
+(define (let-body exp)
+  (cddr exp))
+
+; get the parameters part of the definitions clause from within the let construct
+(define (let-parameters definitions-clause)
+  (map car definitions-clause))
+
+; get the actual argument expressions from the definitions clause from within the let construct
+(define (let-argument-expressions definitions-clause)
+  (map cadr definitions-clause))
+
+; this procedure is used in `eval` when converting a
+; (define (f x y) (+ x y)) statement
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+; convert the let construct into a pair where the car is the lambda and the cons is a list of the
+; arguments
 (define (let->combination exp)
-    (define definitions-clause (let-definitions-clause exp))
-    (cons
-        (list 'lambda (let-parameters definitions-clause) (let-body exp)) 
-        (let-argument-expressions definitions-clause))
-)
+  (define definitions-clause (let-definitions-clause exp))
+  (cons (make-lambda (let-parameters definitions-clause) (let-body exp))
+        (let-argument-expressions definitions-clause)))
 
 (define (tagged-list? exp val)
-    (and (pair? exp) (eq? (car exp) val)))
+  (and (pair? exp) (eq? (car exp) val)))
 
-(define (let? exp) (tagged-list? exp 'let))
+(define (let? exp)
+  (tagged-list? exp 'let))
 
 ; To add it into the eval, we would have to place the following line
 ; before the `application?` cond clause
 ; ((let? exp) (eval (let->combination exp) env))
-
--- chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/tests/4.6.test.rkt
+++ chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/tests/4.6.test.rkt
@@ -2,11 +2,17 @@
 (require rackunit)
 (require "../4.6.rkt")
 
-(define simple-let-expression '(let ((x 1) (y 2)) (+ x y)))
-(check-equal? (let->combination simple-let-expression) 
-    (cons
-        (list 'lambda '(x y) '(+ x y))
-        (list 1 2)
-    )
-)
+(define simple-let-expression
+  '(let ([x 1]
+         [y 2])
+     (+ x y)))
+(check-equal? (let->combination simple-let-expression)
+              (cons (list 'lambda '(x y) '(+ x y)) (list 1 2)))
 (check-equal? (let? simple-let-expression) #t)
+(define multi-line-body-let-expression
+  '(let ([x 1]
+         [y 2])
+     (display x)
+     (+ x y)))
+(check-equal? (let->combination multi-line-body-let-expression)
+              (cons (list 'lambda '(x y) '(display x) '(+ x y)) (list 1 2)))
