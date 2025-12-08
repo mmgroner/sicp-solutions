e368594128ca5bd506c986f10bb7bfb81dd805f6
mmgroner <mendlegr@gmail.com>
2025-12-08 12:57:21 -0500

    feat: complete 4.9

--- chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/4.9.rkt
+++ chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/4.9.rkt
@@ -1,5 +1,7 @@
 #lang racket
 (provide do->combination)
+(provide for->combination)
+(provide while->combination)
 ; syntax
 ; (do <proc> <condition>)
 ; (while <condition> <proc>)
@@ -47,3 +49,43 @@
               (list 'pred pred))
         '(proc)
         '(if (pred) (loop) 'done)))
+
+(define (for->combination exp)
+  (define seq (cadr exp))
+  (define proc (caddr exp))
+
+  (list 'let 'loop
+        (list (list 'seq seq)
+              (list 'proc proc))
+        '(if (null? seq)
+             'done
+             (begin
+               (proc (car seq))
+               (loop (cdr seq)))))
+  )    
+
+(define (while->combination exp)
+  (define pred (cadr exp))
+  (define proc (caddr exp))
+      
+  (list 'let 'loop
+        (list (list 'pred pred)
+              (list 'proc proc))
+        '(if (pred)
+             (begin
+               (proc)
+               (loop))
+             'done)))
+
+(define (until->combination exp)
+  (define pred (cadr exp))
+  (define proc (caddr exp))
+      
+  (list 'let 'loop
+        (list (list 'pred pred)
+              (list 'proc proc))
+        '(if (not pred)
+             (begin
+               (proc)
+               (loop))
+             'done)))     
--- chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/tests/4.9.test.rkt
+++ chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/tests/4.9.test.rkt
@@ -3,17 +3,63 @@
 (require "../4.9.rkt")
 
 (define do-expression
- '(do (lambda ()
-        (display "LOOP")
+  '(do (lambda ()
+         (display "LOOP")
          (newline))
-    (lambda ()
+     (lambda ()
        #f)))
 (define do-derived
   '(let loop ((proc (lambda ()
-                           (display "LOOP")
-                           (newline)))
+                      (display "LOOP")
+                      (newline)))
               (pred (lambda ()
                       #f)))
      (proc)
      (if (pred) (loop) 'done)))
 (check-equal? (do->combination do-expression) do-derived)
+
+(define for-expression
+  '(for 
+       (1 2 3 4 5)
+     (lambda (elm)
+       (display "Element is: ")
+       (display elm)
+       (newline))
+     ))
+(define for-derived
+  '(let loop (
+              (seq (1 2 3 4 5))
+              (proc (lambda (elm)
+                      (display "Element is: ")
+                      (display elm)
+                      (newline))))
+     (if (null? seq)
+         'done
+         (begin
+           (proc (car seq))
+           (loop (cdr seq))))))
+(check-equal? (for->combination for-expression) for-derived)
+
+(define while-expression
+  '(while (lambda () #f) (lambda () (display "LOOP") (newline))))
+(define while-derived
+  '(let loop
+     ((pred (lambda () #f))
+      (proc (lambda () (display "LOOP") (newline))))
+     (if (pred)
+         (begin
+           (proc)
+           (loop))
+         'done)))
+(check-equal? (while->combination while-expression) while-derived)
+
+(define until-expression
+  '(until (lambda () #t) (lambda () (display "LOOP") (newline))))
+(define until-derived
+  '(let loop ((pred (lambda () #t))
+              (proc (lambda () (display "LOOP") (newline))))
+     (if (not pred)
+         (begin
+           (proc)
+           (loop))
+         'done)))
