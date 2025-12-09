6b7d8c2697fec4715f4267213e9c071c18376fe7
mmgroner <mendlegr@gmail.com>
2025-12-09 10:56:08 -0500

    feat(eval): add 4.1.3 code
    - This is the section entitled "Evaluator Data Structures"

--- chapter-4/eval.scm
+++ chapter-4/eval.scm
@@ -203,6 +203,83 @@
                      (expand-clauses rest))))))
         
             
+; Evaluator Data Structures
 
+; Predicates
+(define (true? x) (not (eq? x false)))
+(define (false? x) (eq? x false))
 
-  
+; Compound Procedures
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-env p) (cadddr p))
+
+; Environments
+
+; An environment is a list of frames
+(define (enclosing-environement env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+
+; Each Frame is a pair os lists
+; A list of variables, and a list of values
+(define (make-frame variables values)
+  (cons variables values))
+
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+
+; Extending environments with a new frame
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+; Looking up a variable
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+              (env-loop (enclosing-environement env)))
+            ((eq? var (car vars)) (car vals))
+            (else (scan (cdr vars) (cdr vals))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame))))))
+    (env-loop env))
+
+; Setting a variable (aka adjusting an existing variable's value)
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+              (env-loop (enclosing-environement env)))
+            ((eq? var (car cars)) (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable: SET!" var)
+        (let ((frame (first-frame env))
+            (scan (frame-variables frame)
+                (frame-values frame))))))
+    (env-loop env))
+
+; Defining a new variable
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars) (add-binding-to-frame! var val frame))
+            ((eq? var (car vars)) (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+          (frame-values frame))))
