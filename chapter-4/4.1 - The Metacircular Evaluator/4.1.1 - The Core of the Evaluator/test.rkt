a2e1249d208aadfc93dae75b311424bcd8772619
mmgroner <mendlegr@gmail.com>
2025-12-09 10:59:13 -0500

    chore: re-organize some exercises to match book structure

--- chapter-4/4.1 - The Metacircular Evaluator/4.1.1 - The Core of the Evaluator/test.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(define (make-let definitions body)
-    (list 'let definitions body))
-
-(define let-exp (make-let '((x 2) (y 3)) (display x) (+ x y))))
-
-(display let-exp)
-
-; (eval let-exp)
